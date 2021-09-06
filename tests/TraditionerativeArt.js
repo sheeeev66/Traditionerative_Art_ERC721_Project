c9b72bcf469a56c3e1a289f420168241f5404484
Sheeeev66 <roi.hakim@gmail.com>
2021-09-06 08:26:49 +0300

    Update TraditionerativeArt.js

--- tests/TraditionerativeArt.js
+++ tests/TraditionerativeArt.js
@@ -1,6 +1,8 @@
-const TraditionerativeArt = artifacts.requre("TraditionerativeArt");
+const TraditionerativeArt = artifacts.require("TraditionerativeArt");
+const tokens = ["1", "2"];
 contract("TraditionerativeArt", (accounts) => {
     let [alice, bob] = accounts;
     it("should be able to mint a new token", async () => {
+        const contractInstance = await TraditionerativeArt.new();
     })
 })
